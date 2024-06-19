0293b4cae7abc1894e0f1187baf967afdddc4b4b
Longjam676 <angoupanba.longjam@brandlym.com>
2024-06-19 14:29:31 +0530

    code added

--- src/App.js
+++ src/App.js
@@ -89,23 +89,25 @@ const App = () => {
   const screenHeight = window.innerHeight
 
   return (
-    <div className={`app xxs:block mdm:flex justify-between ${screenHeight < 695 ? "h-[670px]" : "h-screen"} `} >
+    <div className={`app xxs:block xmd:flex justify-between h-screen `} >
       <div className="xxs:w-full mdm:w-3/4 flex justify-end">
 
-        <section className='xxs:my-16 md:my-32  h-[60vh] xxs:w-full xl:w-5/6 pt-32 xxs:-mr-0 mdm:-mr-20 relative' >
-          <div className={`md:absolute top-0  md:left-64 mdm:left-60 lg:left-72 xl:left-80 xxs:w-[360px] xxs:h-[480px]  mdm:w-[360px] mdm:h-[460px] lg:w-[390px] lg:h-[480px] xxs:m-auto  `}>
-            <h3 className='text-end pr-4 mb-2 text-white text-sm'>Home</h3>
-            <div className='relative h-full'>
-              <video ref={videoRef} className={` w-[96%] h-[96%] object-cover ${hasPhoto ? 'hidden' : ''}`}></video>
-              <canvas ref={photoRef} className={` w-[96%] h-[96%] object-cover  ${hasPhoto ? '' : 'hidden'}`}></canvas>
-              <img src={frameImage} className=" absolute -top-1 -left-2.5 w-full h-full pointer-events-none" alt="frame" />
+        <section className='xxs:my-16 xmd:my-20  md:my-32  h-[60vh] xxs:w-full xmd:w-11/12 xl:w-5/6  xxs:-mr-0 mdm:-mr-20 relative' >
+
+          <div className={`xmd:absolute top-0 xmd:left-52  md:left-64 mdm:left-60 lg:left-72 xl:left-80 xxs:w-[360px] xxs:h-[480px] xmd:w-[140px] xmd:h-[180px]  md:w-[200px] md:h-[300px]  mdm:w-[280px] mdm:h-[380px] lg:w-[300px] lg:h-[400px] xxs:m-auto  `}>
+            <h3 className='text-end  mb-2 text-white text-sm'>Home</h3>
+            <div className='relative h-full w-full'>
+              <video ref={videoRef} className={` w-full h-full object-cover ${hasPhoto ? 'hidden' : ''}`}></video>
+              <canvas ref={photoRef} className={` w-full h-full object-cover  ${hasPhoto ? '' : 'hidden'}`}></canvas>
+              <img src={frameImage} className=" absolute top-0 left-0 w-full h-full pointer-events-none" alt="frame" />
             </div>
           </div>
-          <div className='grid xxs:grid-cols-4 xxs:mt-10 md:mt-0 gap-2 xxs:p-5 mdm:p-0'>
+
+          <div className='grid xxs:grid-cols-4 xxs:mt-10 xmd:mt-1 md:mt-16 mdm:mt-24 gap -2 xxs:p-5 mdm:p-0 '>
             {
               ["CAPTURE", "RETAKE", "POST", "SAVE"].map((val, i) => {
-                return <div key={i} className="border-b-2 border-white   md:mb-4 text-start xxs:col-span-2 md:col-span-4 xxs:text-center md:text-start">
-                  <button className='bg-white py-1 px-7 xxs:w-auto sm:w-48 rounded-tl-3xl rounded-br-3xl text-primary text-3xl font-black border-none'
+                return <div key={i} className="border-b-2 border-white   xmd:mb-4 text-start xxs:col-span-2 xmd:col-span-4 xxs:text-center xmd:text-start">
+                  <button className='bg-white py-1 xmd:px-3 mdm:px-7 xxs:w-auto xmd:w-1/4 mdm:w-48 xmd:rounded-tl-xl xmd:rounded-br-xl  mdm:rounded-tl-3xl mdm:rounded-br-3xl text-primary xmd:text-xs mdm:text-3xl font-black border-none'
                     onClick={() => photoHandler(val)}
                     disabled={val === "SAVE" && !hasPhoto}
                   >{val}</button>
@@ -116,7 +118,7 @@ const App = () => {
 
         </section>
       </div>
-      <div className="xxs:w-full mdm:w-1/4  text-right z-10">
+      <div className="xxs:w-full xmd:w-1/4  text-right z-10">
         <img src={rightImg} alt="rightImg" className=" w-full h-full" />
       </div>
     </div>
